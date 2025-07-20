c7e43125618e85c690fa9c8c7cf6f983ac73c693
DaniilZinoviev06 <daniilzinoviev95@gmail.com>
2025-07-20 16:27:59 +0300

    Initial commit

---
+++ src/main.cpp
@@ -0,0 +1,11 @@
+#include <boost/asio/ip/address.hpp>
+#include <iostream>
+#include "pdn_connection.h"
+#include "control_plane.h"
+#include "data_plane.h"
+
+using std::cerr;
+using std::endl;
+using std::cout;
+
+int main() { return 0; }
